1fe706e6667b7843b55c11f975736af784ea2a8c
KirSpaceB <productivekirk@gmail.com>
2023-04-26 14:00:15 -0700

    feat: Rough complete vision of the dashboard page

--- frontend/src/DashboardPage/DashboardPage.tsx
+++ frontend/src/DashboardPage/DashboardPage.tsx
@@ -1,16 +1,25 @@
 import StocksCard from "./DashboardCards/StocksCard"
 import CryptoCard from "./DashboardCards/CyptoCard"
 import { cardProps } from "./cardsData"
+import SideBar from "./DashboardSideBar/SideBar"
+import NewsCard from "./DashboardNewsCard/NewsCard"
 
 const DashboardPage = () => {
   return (
     <div>
+
       {/* If if I margin here it margins from the page why??? */}
-      <div className="flex ml-10">
+      <div className="flex">
+        <div>
+          <SideBar/>
+        </div>
         {/* What should I name this prop? */}
         <StocksCard cardPropsObject={cardProps}/>
         <CryptoCard cryptoCardProp={cardProps}/>
 
+      </div>
+      <div className="flex h-64 bg-pc3 justify-center content-center">
+        <NewsCard/>
       </div>
 
     </div>
---
+++ frontend/src/DashboardPage/DashboardSideBar/SideBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div>
+      <div className='h-96 w-16 bg-pc2 mr-10'>
+        SideBar
+      </div>
+    </div>
+  )
+}
+
+export default SideBar
