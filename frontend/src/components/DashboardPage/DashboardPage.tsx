6cc9b2676d1b454d99da6e94c18ab01ad1549c64
KirSpaceB <productivekirk@gmail.com>
2023-04-29 12:36:08 -0700

    test:'check if env file is working for api calls'

--- frontend/src/components/DashboardPage/DashboardPage.tsx
+++ frontend/src/components/DashboardPage/DashboardPage.tsx
@@ -3,11 +3,24 @@ import {CryptoCard} from "./Cards/CryptoCard"
 import { JournalCard } from "./Cards/JournalCard/JournalCard"
 
 import SideBar from "./SideBar/SideBar"
-import NewsCard from "./NewsCard/NewsCard"
 import cardsData from '../../assets/cardsData.json'
+import { NewsCard } from "../shared/NewsCard/NewsCard"
+import { getNewsAPITest } from "../../services/NewsAPI/testfetch"
+import { useEffect, useState } from "react"
 
 //Read code from time to time for wtfs momments ;o
 const DashboardPage = () => {
+  const [getNewsData, setNewsData] = useState([])
+  useEffect(() => {
+    async function fetchData() {
+      const dataFromNewsAPI = await getNewsAPITest();
+      console.log(dataFromNewsAPI.results);
+      const displayNewAPIData = dataFromNewsAPI.results;
+      setNewsData(displayNewAPIData);
+    }
+    fetchData();
+  }, []);
+  
   return (
     <div>
 
@@ -20,10 +33,16 @@ const DashboardPage = () => {
         <JournalCard journalCardTitle={cardsData['journalTitle']}/>
         <StocksCardFromIndex stocksCardTitle={cardsData['stockTitle']}/>
         <CryptoCard cryptoCardTitle={cardsData['cryptoTitle']}/>
-
+        <div>{getNewsData.map((newsData,index) => (
+            <div key={index}>
+              <h3>{newsData.title}</h3>
+              <p>{newsData.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="flex content-center justify-center h-64 bg-pc3">
-        <NewsCard/>
+        <NewsCard />
       </div>
 
     </div>
--- frontend/src/components/shared/NewsCard.tsx
+++
@@ -1,2 +0,0 @@
-// Since this use more than once we can put it the shared
-// We can create our own 'bootstrap' type compoenents and put and here
