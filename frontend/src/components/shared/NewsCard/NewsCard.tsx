24bd2069a6d8081797784965cd7ffafb5b666366
KirSpaceB <productivekirk@gmail.com>
2023-04-30 17:17:23 -0700

    fix:NewsCard display correct data from getNewsAPI

--- frontend/src/components/shared/NewsCard/NewsCard.tsx
+++ frontend/src/components/shared/NewsCard/NewsCard.tsx
@@ -1,30 +1,29 @@
 import { useEffect, useState } from "react"
 import { getNewsAPI } from "../../../services/NewsAPI/getNewsAPI"
-
+import { propsFromNewsAPI } from "../../../services/NewsAPI/types"
 export const NewsCard = () => {
-  const [getNews, setGetNews] = useState([{
-    newsTitle:'',
-    newsImageUrl:''
-  }])
+  const [dataFromNewsFetchNews, setDataFromNewsFetchNews] = useState<propsFromNewsAPI>({
+    title:'',
+    link:'',
+    pubDate:'',
+  })
   useEffect(() => {
     const fetchNewsData = async () => {
-      const getNewsFromAPI = await getNewsAPI();
-      const title = getNewsFromAPI.results[9].title
-      const imageUrl = getNewsFromAPI.results[9].image_url
-      setGetNews([{newsTitle:title, newsImageUrl:imageUrl}])      
+      const dataToDisplayFromNewsAPI = await getNewsAPI();
+      console.log(dataToDisplayFromNewsAPI);
+      setDataFromNewsFetchNews(dataToDisplayFromNewsAPI)
     };
     fetchNewsData()
   },[])
-
+  
+  const {title,link,pubDate} = dataFromNewsFetchNews;
+  
   return (
     <div className="flex justify-center cotent-center">
       <div className="w-4/6 h-72 bg-pc3">
-        {getNews.map((news,index) => (
-          <div key={index}>
-            {news.newsTitle}
-            <a href={news.newsImageUrl}>Image Url Pawg</a>
-          </div>
-        ))}
+        <h3>{title}</h3>
+        <a href={link} target="_blank">Link to Article</a>
+        <h2>Published Date {pubDate}</h2>
       </div>
     </div>
   )
--- frontend/src/services/NewsAPI/getNewsAPI.ts
+++ frontend/src/services/NewsAPI/getNewsAPI.ts
@@ -1,9 +1,18 @@
 const NEWS_IO_API_KEY = import.meta.env.VITE_NEW_IO_API_KEY
+import { propsFromNewsAPI } from "./types";
 export async function getNewsAPI() {
-  const response = await fetch(`https://newsdata.io/api/1/news?apikey=${NEWS_IO_API_KEY}&q=Tesla&country=us&language=en`//Remove This to allow API to work again//, {
+  const responseFromNewsAPI = await fetch(`https://newsdata.io/api/1/news?apikey=${NEWS_IO_API_KEY}&q=Tesla&country=us&language=en`/, {
     method:'GET',
   });
 
-  const jsonData = await response.json();
-  return jsonData
+  const newsAPIResponseObject = await responseFromNewsAPI.json();
+  const titlePropNewsAPI = newsAPIResponseObject.results[2].title;
+  const linkPropNewsAPI = newsAPIResponseObject.results[2].link;
+  const pubDatePropNewsAPI = newsAPIResponseObject.results[2].pubDate;
+  const newsAPIPropsDisplayedInNewsCard:propsFromNewsAPI = {
+    title:titlePropNewsAPI,
+    link:linkPropNewsAPI,
+    pubDate:pubDatePropNewsAPI
+  }
+  return newsAPIPropsDisplayedInNewsCard
 }
