4b7e96e715ae106d4cc30fea716470ba9a42cf70
KirSpaceB <productivekirk@gmail.com>
2023-05-07 14:32:47 -0700

    feat: created and styled form for JournalPage

--- frontend/src/components/JournalPage/JournalPage.tsx
+++ frontend/src/components/JournalPage/JournalPage.tsx
@@ -1,8 +1,41 @@
+import { ChangeEvent, useState } from "react"
 
 export const JournalPage = () => {
+  const [getInputValue, setInputValue] = useState('');
+  const [saveInputValue, setSavedInputValue] = useState<string[]>([])
+  function inputHandler(e:ChangeEvent<HTMLInputElement>) {
+    setInputValue(e.target.value);
+  };
+  function submitHandler(e:React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setSavedInputValue(prevArray => [...prevArray, getInputValue])
+  };
+  console.log(saveInputValue);
+  
   return (
-    <div>
-      Journal Page
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full p-8 bg-white rounded-lg shadow-md sm:w-3/4 md:w-1/2 lg:w-1/3">
+        <form action="" onSubmit={(e) => submitHandler(e)} className="space-y-4">
+          <input
+            type="text"
+            name=""
+            id=""
+            onChange={(e) => inputHandler(e)}
+            className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+            placeholder="Enter text"
+          />
+          <button className="w-full p-2 text-white bg-indigo-500 rounded-lg hover:bg-indigo-600">
+            Button
+          </button>
+        </form>
+        <div className="mt-8 space-y-2">
+          {saveInputValue.map((items, index) => (
+            <li key={index} className="p-2 bg-gray-200 rounded-lg">
+              {items}
+            </li>
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
