ffd922eea625657abcc4e5995fbc22378bc3ce34
KirSpaceB <productivekirk@gmail.com>
2023-06-08 09:10:36 -0700

    feat: installed D3.js

--- frontend/src/components/StocksPage/DisplayStocks/DisplayStocks.tsx
+++ frontend/src/components/StocksPage/DisplayStocks/DisplayStocks.tsx
@@ -1,52 +1,74 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,useRef } from "react";
 import { getAlphaVantage } from "../../../services/AplhaVantageApi/getAlphaVantage";
-type MetaData = {
-  '1. Information': string,
-  '2. Symbol': string,
-  '3. Last Refreshed': string,
-  '4. Interval': string,
-  '5. Output Size': string,
-  '6. TimeZone':string
-};
-
-type TimeSeriesDataPoint = {
-  'open': string,
-  'high': string,
-  'low': string,
-  'close': string,
-  'volume': string,
-};
-
-type TimeSeries = {
-  [timestamp: string]: TimeSeriesDataPoint,
-};
-
-type APIResponse = {
-  'Meta Data': MetaData,
-  'Time Series (5min)': TimeSeries,
-};
+import { AlphaVantageAPIType } from "../../../services/AplhaVantageApi/getAlphaVantage";
+import * as d3 from "d3";
 
 export const DisplayStocks = () => {
-  const [displayStocks, setDisplayStocks] = useState<APIResponse>();
+  const [displayStocks, setDisplayStocks] = useState<any>();
+  const d3Container = useRef(null); // Ref for D3 container
+  const generateFakeData = () => {
+    const startDate = new Date('2023-06-07T11:30:00');
+    const data = [];
+  
+    for (let i = 0; i < 10; i++) {
+      const time = new Date(startDate.getTime() + i * 5 * 60 * 1000);
+      const open = Math.random() * 1000;
+      const high = open + Math.random() * 100;
+      const low = open - Math.random() * 100;
+      const close = low + Math.random() * (high - low);
+      const volume = Math.floor(Math.random() * 100000);
+  
+      data.push({
+        time,
+        open: open.toFixed(4),
+        high: high.toFixed(4),
+        low: low.toFixed(4),
+        close: close.toFixed(4),
+        volume: volume.toString(),
+      });
+    }
+  
+    return data;
+  };
+
+  const prepareData = () => {
+    if (displayStocks) {
+      const timeSeriesData = displayStocks['Time Series (5min)'];
+      const data = Object.entries(timeSeriesData).map(([time, values]) => ({
+        time,
+        value: parseFloat(values['4. close']),
+      }));
+      return data;
+    }
+    return [];
+  };
 
   useEffect(() => {
     const alphaVantageApiData = async () => {
-      const apiData = await getAlphaVantage();
-      console.log(apiData)
-      setDisplayStocks(apiData)
-      console.log(displayStocks);
+      // const apiData = await getAlphaVantage();
+      setDisplayStocks(generateFakeData());
     };
-    alphaVantageApiData()
+    alphaVantageApiData();
   }, []);
+
+  useEffect(() => {    
+    const container = d3.select(d3Container.current);
+    // Graph dimentions
+    const WIDTH = 500;
+    const HEIGHT = 300;
+    
+    //Append SVG Element to container
+    const SVG = container.append("svg").attr("width", WIDTH).attr("height", HEIGHT).style("border","1px solid black");
+    
+  }, [displayStocks]);
+  
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen sm:flex-row">
       <div className="w-[500px] h-[500px] bg-red-300 mb-4 sm:mb-0 sm:mr-4">
-      {displayStocks && Object.entries(displayStocks["Meta Data"]).map(([key, value]) => (
-        <div key={key}>{key}: {value}</div>
-      ))}
+        test
       </div>
-      <div className="w-[500px] h-[500px] bg-red-300">
-        divTwo
+      <div ref={d3Container} className="w-[500px] h-[500px] bg-red-300">
+        {/* The D3 graph will be inserted here */}
       </div>
     </div>
   );
--- frontend/src/services/AplhaVantageApi/getAlphaVantage.ts
+++ frontend/src/services/AplhaVantageApi/getAlphaVantage.ts
@@ -1,13 +1,30 @@
-import { VITE_APLHA_VANTAGE_KEY } from "../../constants";
 const symbol = 'MSFT';
-
-const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${VITE_APLHA_VANTAGE_KEY}`;
+const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${API_KEY}`;
+interface MetaData {
+  "1. Information":string,
+  "2. Symbol":string,
+  "3. Last Refreshed": string,
+  "4. Interval":string,
+  "5. Output Size": string,
+  "6. Time Zone":string
+}
+interface TimeSeriesEntry {
+  '1. open': string;
+  '2. high': string;
+  '3. low': string;
+  '4. close': string;
+  '5. volume': string;
+}
+export interface AlphaVantageAPIType {
+  "Meta Data":MetaData,
+  "Time Series (5min)":TimeSeriesEntry
+}
 
 export async function getAlphaVantage() {
   const response = await fetch(url, {
     method:'GET'
   });
-  const responseData = await response.json();
+  const responseData: AlphaVantageAPIType = await response.json();
   console.log(responseData);
   return responseData
 }
