8f5387d607e707e8a54054508e3b383bfee129cc
KirSpaceB <productivekirk@gmail.com>
2023-06-20 11:55:10 -0700

    feat: Finished the inputbar component

--- frontend/src/components/StocksPage/DisplayStockTicker.tsx
+++ frontend/src/components/StocksPage/DisplayStockTicker.tsx
@@ -4,9 +4,13 @@ import { InputBar } from "../shared/InputBar/InputBar";
 export const DisplayStockTicker = () => {
   
   return (
-    <div className="mr-4">
-      <p>We put the tickername in the input and it pops out on the graph</p>
+    <div className="mr-10 flex flex-col items-center justify-center">
       <InputBar/>
+      <div className="mt-4">
+        <p>TickerName</p>
+        <p>24H High</p>
+        <p>24H Low</p>
+      </div>
     </div>
   )
 }
--- frontend/src/components/shared/InputBar/InputBar.tsx
+++ frontend/src/components/shared/InputBar/InputBar.tsx
@@ -1,9 +1,18 @@
+import React, {useState, FormEvent} from "react";
 export const InputBar = () => {
+  const [tickerName, setTickerName] = useState<string>("");
 
+  const handleSubmit = (e : FormEvent) => {
+    e.preventDefault();    
+    console.log(tickerName);
+  };
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTickerName(event.target.value);
+  };
   return (
-    <div className="flex items-center justify-center">
+    <div className="">
       <div className="relative w-1/2">
-        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-chart-dots absolute left-2 top-1/2 transform -translate-y-1/2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-chart-dots absolute left-2 top-5 transform -translate-y-1/2" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
           <path d="M3 3v18h18" />
           <path d="M9 9m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
@@ -12,7 +21,32 @@ export const InputBar = () => {
           <path d="M10.16 10.62l2.34 2.88" />
           <path d="M15.088 13.328l2.837 -4.586" />
         </svg>
-        <input type="text" placeholder="Ticker Name" className='w-full h-10 border-gray-300 rounded-md focus:border-indigo-500 focus:outline-none pl-10'/>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Ticker Name"
+            className='w-64 h-10 border-gray-300 rounded-md focus:border-indigo-500 focus:outline-none pl-10'
+            value={tickerName}
+            onChange={handleChange}
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-search absolute left-56 top-5 transform -translate-y-1/2 cursor-pointer"
+            width="22"
+            height="24"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#2c3e50"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            onClick={handleSubmit}
+            >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+            <path d="M21 21l-6 -6" />
+          </svg>
+        </form>
       </div>
     </div>
   )
